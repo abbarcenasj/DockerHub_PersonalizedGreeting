3288c917dd16aa074b3d9daf7f2177022e142ef2
EC2 Default User <ec2-user@ip-172-31-12-233.ec2.internal>
2020-03-01 23:45:55 +0000

    some changes to docker project

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 import click
 
 @click.command()
-def hello():
-    click.echo('Hello World!')
+@click.option('--name', prompt='Welcome! What is your name?',
+              help='The person to greet.')
+              
+def hello(name):
+    click.echo('Hello %s!' % (name))
     
 if __name__ == '__main__':
     hello()
